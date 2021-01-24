0c0638ee4243768e015e2261f0f45b2fa61e2c57
Kramer <zyr24615@bcaoo.com>
2021-01-24 12:07:14 +1100

    Орегинал

---
+++ Test4/Program.cs
@@ -0,0 +1,33 @@
+﻿using RestSharp;
+using System.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServicePointManager.SecurityProtocol |= SecurityProtocolType.Tls12; // <-- В коде не где не используеться.
+
+            RestClient Client = new RestClient();
+            RestRequest Request = new RestRequest("https://nick-kramer117.github.io");
+            IRestResponse Result = Client.Execute(Request);
+            string PageHTMLSourceCode = Result.Content;
+
+            if (Result.StatusCode != HttpStatusCode.OK)
+            {
+                Console.WriteLine("Невозможно скачать страницу. Что-то пошло не так.");
+                return;
+            }
+
+            Console.Write(PageHTMLSourceCode);
+            Console.ReadKey();
+        }
+    }
+}
